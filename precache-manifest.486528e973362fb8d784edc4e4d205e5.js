021baf8f0f3da0690e0349d398ece7e834839fa4
Kevin Eslick <eslick.kevin@yahoo.com>
2020-10-06 22:24:04 -0500

    Updates

---
+++ precache-manifest.486528e973362fb8d784edc4e4d205e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1698e73325dd9613857ab3dca6a63a65",
+    "url": "/cdc-research/index.html"
+  },
+  {
+    "revision": "ea81aef67d67d7140bbf",
+    "url": "/cdc-research/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87432ad6498a68159c78",
+    "url": "/cdc-research/static/js/2.11224a5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cdc-research/static/js/2.11224a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea81aef67d67d7140bbf",
+    "url": "/cdc-research/static/js/main.0ab7f282.chunk.js"
+  },
+  {
+    "revision": "9edb16ff2588f54ce15e",
+    "url": "/cdc-research/static/js/runtime-main.b2b9c24e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdc-research/static/media/logo.5d5d9eef.svg"
+  }
+]);
